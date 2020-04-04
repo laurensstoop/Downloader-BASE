261883d15df30e7f905e8653023e4bb53590dcc5
Laurens Stoop <l.p.stoop@uu.nl>
2020-04-04 03:35:46 +0200

    Added CFSR and it now works

--- src/CFSR_set-BASE.sh
+++ src/CFSR_set-BASE.sh
@@ -3,65 +3,92 @@
 # 
 
 
-tmp2m.gdas.197901.grb2
-dswsfc.gdas.198008.grb2
-wnd10m.gdas.198302.grb2
-
-
-### Run in the origin folder the following commandline
-for filename in ./origin/* ;do
-   cdo -f nc copy -sellonlatbox,-31,74.25,34.5,80 -selparam,-1 $filename ./europe/$filename
-done 
-
-
-
-
-
-
-
-
-
-### Run in the europe folder
-for Y in $(seq 1836 2015);do 
-    # Fix the variables for the radiation
-    cdo chname,dswrf,ssrd dswrf.sfc.$Y.nc /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-
+# Radiation
+### Run in the origin folder
+months='01 02 03 04 05 06 07 08 09 10 11 12'
+for Y in $(seq 1979 2010);do  #1979 2010 
+    for M in $months ; do
+        # Convert from Grib2 data to netcdf
+        cdo -f nc copy -sellonlatbox,-31,74.25,34.5,80 /media/stoop/DataFiles/CFSR/origin/dswsfc.gdas.$Y$M.grb2 /media/stoop/DataStager/TEMP/CFSR/dswsfc.gdas.$Y$M.nc
+        # Fix the variables for the radiation
+        cdo chname,dswrf,ssrd /media/stoop/DataStager/TEMP/CFSR/dswsfc.gdas.$Y$M.nc /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y$M.nc
+    done
+    # Merge the time 
+    cdo mergetime /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y*.nc /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
     # Rename the attributes and set the variables attributes for windspeed
-    ncatted -O -a map_area,global,o,c,"Europe" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-    ncatted -O -a experiment,global,o,c,"Model: 20CR" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-    ncatted -O -a author,global,a,c,"; Laurens Stoop, UU/KNMI/TenneT" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-    ncatted -O -a email,global,a,c,"; l.p.stoop@uu.nl" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-    ncatted -O -a long_name,ssrd,o,c,"Downward short-wave radiation flux masked for European region" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-    ncatted -O -a short_name,ssrd,o,c,"ssrd" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
-    ncatted -O -a history,global,d,, -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_ssrd_$Y.nc
+    ncatted -O -a map_area,global,o,c,"Europe" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
+    ncatted -O -a experiment,global,o,c,"Model: CFSR" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
+    ncatted -O -a author,global,a,c,"; Laurens Stoop, UU/KNMI/TenneT" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
+    ncatted -O -a email,global,a,c,"; l.p.stoop@uu.nl" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
+    ncatted -O -a long_name,ssrd,o,c,"Downward short-wave radiation flux masked for European region" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
+    ncatted -O -a short_name,ssrd,o,c,"ssrd" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
+    ncatted -a history,global,d,, -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc /media/stoop/DataFiles/CFSR/CFSR-EU_BASE/CFSR-EU_ssrd_$Y.nc
+    # Clean up 
+    rm /media/stoop/DataFiles/CFSR/origin/dswsfc.gdas.$Y*.grb2
+    rm /media/stoop/DataStager/TEMP/CFSR/dswsfc.gdas.$Y*.nc
+    rm /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y*.nc
+    rm /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_ssrd_$Y.nc
 done
 
-for Y in $(seq 1836 2015);do 
-    # Fix the variables for the temperature
-    cdo chname,air,t2m -addc,-273.15 air.2m.$Y.nc /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
 
+
+# temperature
+### Run in the origin folder
+months='01 02 03 04 05 06 07 08 09 10 11 12'
+for Y in $(seq 1979 2010);do  #1979 2010 
+    for M in $months ; do
+        # Convert from Grib2 data to netcdf
+        cdo -f nc copy -sellonlatbox,-31,74.25,34.5,80 /media/stoop/DataFiles/CFSR/origin/tmp2m.gdas.$Y$M.grb2 /media/stoop/DataStager/TEMP/CFSR/tmp2m.gdas.$Y$M.nc
+        # Fix the variables for the radiation
+        cdo chname,2t,t2m -addc,-273.15 /media/stoop/DataStager/TEMP/CFSR/tmp2m.gdas.$Y$M.nc /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y$M.nc
+    done
+    # Merge the time 
+    cdo mergetime /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y*.nc /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
     # Rename the attributes and set the variables attributes for windspeed
-    ncatted -O -a map_area,global,o,c,"Europe" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a experiment,global,o,c,"Model: 20CR" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a author,global,a,c,"; Laurens Stoop, UU/KNMI/TenneT" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a email,global,a,c,"; l.p.stoop@uu.nl" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a long_name,t2m,o,c,"Temperature at 2 meter masked for European region" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a short_name,t2m,o,c,"t2m" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a units,t2m,o,c,"degC" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
-    ncatted -O -a history,global,d,, -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_t2m_$Y.nc
+    ncatted -O -a map_area,global,o,c,"Europe" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -O -a experiment,global,o,c,"Model: CFSR" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -O -a author,global,a,c,"; Laurens Stoop, UU/KNMI/TenneT" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -O -a email,global,a,c,"; l.p.stoop@uu.nl" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -O -a long_name,t2m,o,c,"Temperature at 2 meter masked for European region" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -O -a short_name,t2m,o,c,"t2m" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -O -a units,t2m,o,c,"degC" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
+    ncatted -a history,global,d,, -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc /media/stoop/DataFiles/CFSR/CFSR-EU_BASE/CFSR-EU_t2m_$Y.nc
+    # Clean up 
+    rm /media/stoop/DataFiles/CFSR/origin/tmp2m.gdas.$Y*.grb2
+    rm /media/stoop/DataStager/TEMP/CFSR/tmp2m.gdas.$Y*.nc
+    rm /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y*.nc
+    rm /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_t2m_$Y.nc
 done
 
-for Y in $(seq 1836 2015);do 
-    # Fix the variables for the windspeed
-    cdo chname,uwnd,wspd -sqrt -add -sqr uwnd.10m.$Y.nc -sqr vwnd.10m.$Y.nc.nc /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
+
+
+# wind speed
+### Run in the origin folder
+months='01 02 03 04 05 06 07 08 09 10 11 12'
+for Y in $(seq 1979 2010);do  #1979 2010 
+    for M in $months ; do
+        # Convert from Grib2 data to netcdf
+        cdo -f nc copy -sellonlatbox,-31,74.25,34.5,80 /media/stoop/DataFiles/CFSR/origin/wnd10m.gdas.$Y$M.grb2 /media/stoop/DataStager/TEMP/CFSR/wnd10m.gdas.$Y$M.nc
+        # Fix the variables for the radiation
+        cdo 'expr,wspd=sqrt(10u^2+10v^2)' /media/stoop/DataStager/TEMP/CFSR/wnd10m.gdas.$Y$M.nc /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y$M.nc
+    done
+    # Merge the time 
+    cdo mergetime /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y*.nc /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
     # Rename the attributes and set the variables attributes for windspeed
-    ncatted -O -a map_area,global,o,c,"Europe" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
-    ncatted -O -a experiment,global,o,c,"Model: 20CR" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
-    ncatted -O -a author,global,a,c,"; Laurens Stoop, UU/KNMI/TenneT" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
-    ncatted -O -a email,global,a,c,"; l.p.stoop@uu.nl" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
-    ncatted -O -a long_name,wspd,o,c,"Windspeed at 10 meter masked for European region" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
-    ncatted -O -a short_name,wspd,o,c,"wspd" -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
-    ncatted -O -a history,global,d,, -h /media/stoop/DataFiles/20CR/20CR-EU_BASE/20CR-EU_wspd_$Y.nc
+    ncatted -O -a map_area,global,o,c,"Europe" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -O -a experiment,global,o,c,"Model: CFSR" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -O -a author,global,a,c,"; Laurens Stoop, UU/KNMI/TenneT" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -O -a email,global,a,c,"; l.p.stoop@uu.nl" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -O -a long_name,wspd,o,c,"Wind speed at 10 meter masked for European region" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -O -a short_name,wspd,o,c,"wspd" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -O -a units,wspd,o,c,"m s**-1" -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
+    ncatted -a history,global,d,, -h /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc /media/stoop/DataFiles/CFSR/CFSR-EU_BASE/CFSR-EU_wspd_$Y.nc
+    # Clean up 
+    rm /media/stoop/DataFiles/CFSR/origin/wnd10m.gdas.$Y*.grb2
+    rm /media/stoop/DataStager/TEMP/CFSR/wnd10m.gdas.$Y*.nc
+    rm /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y*.nc
+    rm /media/stoop/DataStager/TEMP/CFSR/CFSR-EU_wspd_$Y.nc
 done
+
 
 
